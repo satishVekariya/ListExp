2b1c3877d834858162a630619d51f0e82bcc9cd9
satish <satish.vekaariya@gamesys.com>
2024-01-27 19:07:49 +0000

    Initial Commit

---
+++ ListExp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ListExp
+//
+//  Created by Satish Vekariya on 27/01/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ListExp/ListExpApp.swift
@@ -0,0 +1,17 @@
+//
+//  ListExpApp.swift
+//  ListExp
+//
+//  Created by Satish Vekariya on 27/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct ListExpApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
